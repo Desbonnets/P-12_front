4183535b8dcec1953c0e60f1352869182d7e22d6
Desbonnets <gillesdesbonnets@gmail.com>
2023-12-30 13:18:44 +0100

    readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # P-12_front
+
+Pour lancer le projet, il faut faire un `npm install` pour avoir toutes les library. 
+
+Ensuite dans le fichier `package.json` vous pouvez changez le port sur la lingne `"start": "set PORT=3001 && react-scripts start",`.
+
+Pour finir lancer le projet avec la commande `($env:REACT_APP_URL_API = "http://localhost:3000") -and (npm start)` vous pouvez changez avec l'url de votre api.
--- src/api/api.js
+++ src/api/api.js
@@ -8,7 +8,7 @@ import { fetchData } from "./fetch";
  * @returns {any} Retourne la reponse de l'api
  */
 export async function getUserData(userId, isApi, navigate) {
-  const response = isApi ? await fetchData(`http://localhost:3000/user/${userId}`, navigate) : MockDataUser(userId);
+  const response = isApi ? await fetchData(process.env.REACT_APP_URL_API+`/user/${userId}`, navigate) : MockDataUser(userId);
   return response;
 }
 
@@ -21,7 +21,7 @@ export async function getUserData(userId, isApi, navigate) {
  */
 export async function getUserActivityData(userId, isApi, navigate) {
 
-  const response = isApi ? await fetchData(`http://localhost:3000/user/${userId}/activity`, navigate) : MockDataActivity(userId);
+  const response = isApi ? await fetchData(process.env.REACT_APP_URL_API+`/user/${userId}/activity`, navigate) : MockDataActivity(userId);
   return response;
 }
 
@@ -34,7 +34,7 @@ export async function getUserActivityData(userId, isApi, navigate) {
  */
 export async function getUserSessionData(userId, isApi, navigate) {
 
-  const response = isApi ? await fetchData(`http://localhost:3000/user/${userId}/average-sessions`, navigate) : MockDataAverageSessions(userId);
+  const response = isApi ? await fetchData(process.env.REACT_APP_URL_API+`/user/${userId}/average-sessions`, navigate) : MockDataAverageSessions(userId);
   return response;
 }
 
@@ -47,6 +47,6 @@ export async function getUserSessionData(userId, isApi, navigate) {
  */
 export async function getUserPerformanceData(userId, isApi, navigate) {
 
-  const response = isApi ? await fetchData(`http://localhost:3000/user/${userId}/performance`, navigate) : MockDataPerformance(userId);
+  const response = isApi ? await fetchData(process.env.REACT_APP_URL_API+`/user/${userId}/performance`, navigate) : MockDataPerformance(userId);
   return response;
 }
